7c1726ab3aac7923cf9d7392f51aac57982ba5c6
Adam <adam.kowalczuk@poczta.fm>
2021-03-04 21:46:25 +0100

    dodano konfigurację z CMS

--- src/templates/articleTemplate.js
+++ src/templates/articleTemplate.js
@@ -1,17 +1,17 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import "../styles/articleTemplate.scss"
 
 const ArticleTemplate = ({ pageContext: { data } }) => (
   <Layout>
+    <SEO title={data.title} />
     <Link to="/">Go back</Link>
     <h1>{data.title}</h1>
     <h2>{data.author}</h2>
     <p>{data.description}</p>
-    <img src={data.image.url} />
+    <img src={data.image.url} alt={data.title} />
   </Layout>
 )
 
