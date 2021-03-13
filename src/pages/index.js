05b9ae42b336188e60a5bce698fcd1a9f88aec7a
Adam <adam.kowalczuk@poczta.fm>
2021-03-13 13:10:00 +0100

    test dostępu do ścieżek

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,55 +1,5 @@
-// import * as React from "react"
-// import { Link, graphql } from "gatsby"
-// import Layout from "../components/layout"
-// import SEO from "../components/seo"
-
-// const IndexPage = ({
-//   data: {
-//     allSitePage: { edges },
-//   },
-// }) => (
-//   <Layout>
-//     <SEO title="Strona główna" />
-//     {edges.map(page => (
-//       <Link
-//         style={{ display: "block" }}
-//         key={page.node.path}
-//         to={page.node.path}
-//       >
-//         {page.node.context.data.title}
-//       </Link>
-//     ))}
-//   </Layout>
-// )
-
-// export const allArticlePagesQuery = graphql`
-//   {
-//     allSitePage(
-//       filter: {
-//         component: {
-//           eq: "C:/Users/Adam/Desktop/Projekty/motivational-blog/src/templates/articleTemplate.js"
-//         }
-//       }
-//     ) {
-//       edges {
-//         node {
-//           path
-//           component
-//           context {
-//             data {
-//               title
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// `
-
-// export default IndexPage
-
 import * as React from "react"
-import { Link, graphql, StaticQuery } from "gatsby"
+import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import "../styles/historie.scss"
