6c0d7759833ca0f7020861562c2e95d8406aadc4
Adam <adam.kowalczuk@poczta.fm>
2021-03-13 13:55:51 +0100

    test dostępu do ścieżek

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,10 @@ const IndexPage = ({
     <SEO title="Strona główna" />
     {edges.map(page => (
       <>
+        {console.log(page.node.component)}
+        {console.log(page.node.context.data.title)}
+        {console.log(page)}
+        <h1>Hej</h1>
         <h1>{page.node.component}</h1>
         <div className="history-container" key={page.node.context.data.title}>
           <h1>{page.node.context.data.title}</h1>
@@ -28,9 +32,6 @@ const IndexPage = ({
           </button>
           {page.node.path}
         </div>
-        {console.log(page.node.component)}
-        {console.log(page.node.context.data.title)}
-        {console.log(page)}
       </>
     ))}
   </Layout>
